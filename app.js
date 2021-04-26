4d7209d39d89757bcb3e7981b1de96db81fb3659
r0ck1n70sh <r0ck1n70sh@gmail.com>
2021-04-26 16:41:26 +0530

    heroku deploy

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path');
+const app = express();
+const router = express.Router();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+app.get('/style.css', (req, res) => {
+    res.sendFile(path.join(__dirname+'/style.css'));
+})
+
+app.get('/script.js', (req, res) => {
+    res.sendFile(path.join(__dirname+'/script.js'));
+})
+
+app.use("/", router);
+app.listen(process.env.port || 3000, () => console.log('server ready...'))
